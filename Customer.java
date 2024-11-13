dfe4c5fa967565c39bbaf34db5f570ccc108d96e
Bacha Wondimu <91438094+BachaWondimu@users.noreply.github.com>
2024-11-13 15:33:40 -0600

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,67 +1,36 @@
-
 public class Customer {
-	
-	
-	
-	private String name;
-	private int age;
-	
-	
-	
-	
-	/**
-	 * @param name
-	 * @param age
-	 */
-	public Customer(String name, int age) {
-		this.name = name;
-		this.age = age;
-	}
-	
-	
-	//Copy Constructor
-	public Customer(Customer c) {
-		this.name = c.name;
-		this.age = c.age;
-	}
 
+    private String name;
+    private int age;
 
-	@Override
-	public String toString() {
-		return "Customer [name=" + name + ", age=" + age + "]";
-	}
+    public Customer(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
+    public Customer(Customer c) {
+        this.name = c.name;
+        this.age = c.age;
+    }
 
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
+    @Override
+    public String toString() {
+        return "Customer [name=" + name + ", age=" + age + "]";
+    }
 
+    public String getName() {
+        return name;
+    }
 
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public int getAge() {
+        return age;
+    }
 
-	/**
-	 * @return the age
-	 */
-	public int getAge() {
-		return age;
-	}
-
-
-	/**
-	 * @param age the age to set
-	 */
-	public void setAge(int age) {
-		this.age = age;
-	}
-	
-	
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
