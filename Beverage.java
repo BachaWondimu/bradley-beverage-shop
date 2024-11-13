6e1f85811bc70c08fc7961ed2939fc652285d77b
Bacha Wondimu <91438094+BachaWondimu@users.noreply.github.com>
2024-11-13 15:29:00 -0600

    Update Beverage.java

--- Beverage.java
+++ Beverage.java
@@ -2,98 +2,70 @@ import java.util.Objects;
 
 public abstract class Beverage {
 
-	
-	private String bevName;
-	public TYPE beverageType;
-	public SIZE beverageSize;
-	final public double BASEPRICE = 2.0;
-	final public int SIZEPRICE = 1;
-	
+    private String bevName;
+    public TYPE beverageType;
+    public SIZE beverageSize;
+    final public double BASEPRICE = 2.0;
+    final public int SIZEPRICE = 1;
 
-	public Beverage(String bevName, TYPE beverageType, SIZE beverageSize) {
-		this.bevName = bevName;
-		this.beverageType = beverageType;
-		this.beverageSize = beverageSize;
-	}
+   
+    public Beverage(String bevName, TYPE beverageType, SIZE beverageSize) {
+        this.bevName = bevName;
+        this.beverageType = beverageType;
+        this.beverageSize = beverageSize;
+    }
 
-	
+    
+    public abstract double calcPrice();
 
-	public abstract double calcPrice();
+    @Override
+    public String toString() {
+        return "Beverage Name=" + bevName
+                + ", Beverage Type=" + beverageType
+                + ", Beverage Size=" + beverageSize;
+    }
 
-	
-	@Override
-	public String toString () {
-		return "Beverage Name=" + bevName
-				+ ", Beverage Type=" + beverageType
-				+ ", Beverage Size=" + beverageSize;
-		
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Beverage bev = (Beverage) obj;
+        return Objects.equals(bevName, bev.bevName)
+                && beverageSize == bev.beverageSize
+                && beverageType == bev.beverageType;
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(bevName, beverageType, beverageSize);
+    }
 
+   
+    public String getBevName() {
+        return bevName;
+    }
 
-	
-	public boolean equals(Beverage bev) {
-		if (this == bev)
-			return true;
-		if (bev == null)
-			return false;
-		return Objects.equals(bevName, bev.bevName) 
-				&& beverageSize == bev.beverageSize
-				&& beverageType == bev.beverageType;
-	}
+    public void setBevName(String bevName) {
+        this.bevName = bevName;
+    }
 
+   
+    public TYPE getType() {
+        return beverageType;
+    }
 
+    public void setType(TYPE beverageType) {
+        this.beverageType = beverageType;
+    }
 
-	/**
-	 * @return the beverageName
-	 */
-	public String getBevName() {
-		return bevName;
-	}
+    
+    public SIZE getSize() {
+        return beverageSize;
+    }
 
-
-
-	/**
-	 * @param beverageName the beverageName to set
-	 */
-	public void setBevName (String bevName) {
-		this.bevName = bevName;
-	}
-
-	
-
-	/**
-	 * @return the beverageType
-	 */
-	public TYPE getType() {
-		return beverageType;
-	}
-
-
-
-	/**
-	 * @param beverageType the beverageType to set
-	 */
-	public void setType(TYPE beverageType) {
-		this.beverageType = beverageType;
-	}
-
-
-
-	/**
-	 * @return the beverageSize
-	 */
-	public SIZE getSize() {
-		return beverageSize;
-	}
-
-
-
-	/**
-	 * @param beverageSize the beverageSize to set
-	 */
-	public void setSize(SIZE beverageSize) {
-		this.beverageSize = beverageSize;
-	}
-	
+    public void setSize(SIZE beverageSize) {
+        this.beverageSize = beverageSize;
+    }
 }
